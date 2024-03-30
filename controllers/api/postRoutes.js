97518a549e111cc9d0cf4ae3b9ecf35f3f2c7f75
Ian Hart <ianhart7790@gmail.com>
2024-03-30 14:11:07 -0400

    feat: add descriptive comments

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,10 +3,20 @@ const PostService = require('../../services/PostService');
 const withAuth = require('../../middleware/withAuth');
 const requireTitleAndPostText = require('../../middleware/requireTitleAndPostText');
 
+// delete a post by its id
 router.delete('/:id', withAuth, async (req, res) => {
   try {
+    // get post by id
     const post = await PostService.getPostWithUserById(req.params.id);
 
+    // if post with specified id is not found send error response
+    if (!post) {
+      return res
+        .status(400)
+        .send({ message: 'Bad Request', error: `A post with the id ${req.params.id} was not found` });
+    }
+
+    // check to see if post's user is equal to the session user
     if (!PostService.canActionPost(post, req.session.user_id)) {
       return res.status(403).json({ message: 'Forbidden Action', message: "You cannot delete another user's post" });
     }
@@ -17,20 +27,25 @@ router.delete('/:id', withAuth, async (req, res) => {
   }
 });
 
+// update a post by it's id
 router.put('/:id', withAuth, requireTitleAndPostText, async (req, res) => {
   try {
+    // get post by its id
     const post = await PostService.getPostWithUserById(req.params.id);
 
+    // check to see if post exists
     if (!post) {
       return res
         .status(400)
         .send({ message: 'Bad Request', error: `A post with the id ${req.params.id} was not found` });
     }
 
+    // check to see if post's user is equal to session user
     if (!PostService.canActionPost(post, req.session.user_id)) {
       return res.status(403).json({ message: 'Forbidden Action', error: "You cannot update another user's post" });
     }
 
+    // update post
     post.title = req.body.title;
     post.post_text = req.body.postText;
 
--- services/CommentService.js
+++ services/CommentService.js
@@ -1,6 +1,7 @@
 const { Comment } = require('../models');
 
 class CommentService {
+  // delete comment by id
   static async deleteCommentById(commentId) {
     return await Comment.destroy({
       where: {
@@ -9,14 +10,17 @@ class CommentService {
     });
   }
 
+  // get comment by id
   static async getCommentById(commentId) {
     return await Comment.findByPk(commentId);
   }
 
+  // check to see if comment user is equal to session user
   static canActionComment(comment, userId) {
     return comment.user_id === userId;
   }
 
+  // create a new comment
   static async createComment(data) {
     await Comment.create(data);
   }
